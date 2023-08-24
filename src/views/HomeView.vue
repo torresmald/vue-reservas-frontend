8cc50f119cfdb96cb3a074a33c0dae56981d698a
torresmald <jonathan.torresmald@gmail.com>
2023-08-24 16:18:28 +0200

    route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,26 @@
 <script setup>
-
+import { RouterView, useRoute } from "vue-router";
+const authRoutes = [
+  { name: "register", text: "Registro" },
+  {name: 'olvide-password', text: 'Recuperar Password'},
+  { name: "login", text: "Login" }
+];
+const route = useRoute()
 </script>
 
 <template>
-  <h1>Desde HomeView</h1>
+  <div class="mx-auto lg:w-3/5 mt-20">
+    <RouterView />
+    <nav
+      class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0"
+    >
+      <RouterLink
+        v-for="authRoute in authRoutes"
+        class="uppercase font-bold text-white"
+        :class="{'hidden': route.name === authRoute.name}"
+        :to="{ name: authRoute.name }"
+        >{{ authRoute.text }}
+      </RouterLink>
+    </nav>
+  </div>
 </template>
-
